fe5672904b41026ae5350b135ad675e679fc8a6f
Kerim Sekili <kerimsekilix@gmail.com>
2023-11-19 01:02:14 +0300

    Proxy Set

--- investdashapi/src/main/java/com/investdash/ws/user/UserController.java
+++ investdashapi/src/main/java/com/investdash/ws/user/UserController.java
@@ -1,13 +1,11 @@
 package com.investdash.ws.user;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @CrossOrigin
     @PostMapping("api/v1/users")
     void createUser(){}
 
