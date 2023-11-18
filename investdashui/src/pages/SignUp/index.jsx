5872290ced0caf557856eeda44e028f8ddf25aa6
Kerim Sekili <kerimsekilix@gmail.com>
2023-11-18 23:58:00 +0300

    Interaction with Form

--- investdashui/src/pages/SignUp/index.jsx
+++ investdashui/src/pages/SignUp/index.jsx
@@ -1,24 +1,45 @@
+import { useState } from "react";
+
 export function SignUp() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+
+  console.log(username);
   return (
     <>
       <h1>Sign Up</h1>
       <div>
         <label htmlFor="username">Username</label>
-        <input id="username" />
+        <input
+          id="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" />
+        <input id="email" onChange={(event) => setEmail(event.target.value)} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
+        <input
+          id="password"
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="passwordRepeat">Repeat Password</label>
-        <input id="passwordRepeat" type="password" />
+        <input
+          id="passwordRepeat"
+          type="password"
+          onChange={(event) => setPasswordRepeat(event.target.value)}
+        />
       </div>
-      <button>Sign Up</button>
+      <button disabled={!password || password !== passwordRepeat}>
+        Sign Up
+      </button>
     </>
   );
 }
