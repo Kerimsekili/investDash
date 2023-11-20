c580e24c0426975e231683c5b2ac86c9c6b2ebd6
Kerim Sekili <kerimsekilix@gmail.com>
2023-11-20 22:13:11 +0300

    Email Validation

--- investdashapi/src/main/java/com/investdash/ws/user/UserController.java
+++ investdashapi/src/main/java/com/investdash/ws/user/UserController.java
@@ -17,18 +17,25 @@ public class UserController {
     UserService userService;
 
     @PostMapping("api/v1/users")
-    ResponseEntity<?> createUser(@RequestBody User user){
+    ResponseEntity<?> createUser(@RequestBody User user){ ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation error");
+        apiError.setStatus(400);
+        Map<String,String> validationErrors = new HashMap<>();
         if (user.getUsername() == null || user.getUsername().isEmpty()){
-            ApiError apiError = new ApiError();
-            apiError.setPath("/api/v1/users");
-            apiError.setMessage("Validation error");
-            apiError.setStatus(400);
-            Map<String,String> validationErrors = new HashMap<>();
-            validationErrors.put("username","Username cannot be null");
-            apiError.setValidationErrors(validationErrors);
 
-            return ResponseEntity.badRequest().body(apiError);
+            validationErrors.put("username","Username cannot be null");
+
         }
+        if (user.getEmail() == null || user.getEmail().isEmpty()){
+            validationErrors.put("email","Email cannot be null");
+
+        }
+
+        if (validationErrors.size() > 0){
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);}
+
         userService.save(user);
         return ResponseEntity.ok(new GenericMessage("User is created"));
     }
