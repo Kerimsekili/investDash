d3750e47f72db86fb2a2e95d26686bc9aa77f864
Kerimsekili <kerimsekilix@gmail.com>
2023-12-19 02:08:01 +0300

    Language Support from React

--- investdashui/src/main.jsx
+++ investdashui/src/main.jsx
@@ -4,6 +4,43 @@ import ReactDOM from "react-dom/client";
 import { SignUp } from "./pages/SignUp/index.jsx";
 import "./styles.scss";
 
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: {
+          signUp: "Sign Up",
+          username: "Username",
+          email: "E-mail",
+          password: "Password",
+          passwordRepeat: "Password Repeat",
+          passwordMismatch: "Password Missmatch",
+          genericError: "Unexpected error occured.Please try again",
+        },
+      },
+      tr: {
+        translation: {
+          signUp: "Kayit Ol",
+          username: "Kullanici Adi",
+          email: "E-posta",
+          password: "Sifre",
+          passwordRepeat: "Sifre Tekrar",
+          passwordMismatch: "Sifreniz Eslesmiyor",
+          genericError: "Beklenmedik bir hata olustu, lutfen tekrar deneyin",
+        },
+      },
+    },
+    fallbackLng: "tr",
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SignUp />
--- investdashui/src/pages/SignUp/index.jsx
+++ investdashui/src/pages/SignUp/index.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useMemo, useState } from "react";
 import { signUp } from "./api";
 import { Input } from "./component/Input";
+import { useTranslation } from "react-i18next";
 
 export function SignUp() {
   const [username, setUsername] = useState();
@@ -12,6 +13,7 @@ export function SignUp() {
   const [successMessage, setSuccessMessage] = useState();
   const [errors, setErrors] = useState({});
   const [generalError, setGeneralError] = useState();
+  const { t } = useTranslation();
 
   useEffect(() => {
     setErrors(function (lastErrors) {
@@ -60,7 +62,7 @@ export function SignUp() {
       ) {
         setErrors(axiosError.response.data.validationErrors);
       } else {
-        setGeneralError("Unexpected error occured.Please try again");
+        setGeneralError(t("genericError"));
       }
     } finally {
       setApiProgress(false);
@@ -70,7 +72,7 @@ export function SignUp() {
   const passwordRepeatError = useMemo(() => {
     if (password && password !== passwordRepeat) {
       console.log("always running");
-      return "Password Mismatch";
+      return t("passwordMismatch");
     }
     return "";
   }, [password, passwordRepeat]);
@@ -80,31 +82,31 @@ export function SignUp() {
       <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
         <form className="card" onSubmit={onSubmit}>
           <div className="text-center card-header">
-            <h1>Sign Up</h1>
+            <h1>{t("signUp")}</h1>
           </div>
           <div className="card-body">
             <Input
               id="username"
-              label="Username"
+              label={t("username")}
               error={errors.username}
               onChange={(event) => setUsername(event.target.value)}
             />
             <Input
               id="email"
-              label="E=mail"
+              label={t("email")}
               error={errors.email}
               onChange={(event) => setEmail(event.target.value)}
             />
             <Input
               id="password"
-              label="Password"
+              label={t("password")}
               type="password"
               error={errors.password}
               onChange={(event) => setPassword(event.target.value)}
             />
             <Input
               id="passwordRepeat"
-              label="Password Repeat"
+              label={t("passwordRepeat")}
               error={passwordRepeatError}
               type="password"
               onChange={(event) => setPasswordRepeat(event.target.value)}
@@ -128,7 +130,7 @@ export function SignUp() {
                     aria-hidden="true"
                   ></span>
                 )}
-                Sign Up
+                {t("signUp")}
               </button>
             </div>
           </div>
