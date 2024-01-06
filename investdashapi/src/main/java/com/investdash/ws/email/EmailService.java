d108779df366c57beadb2126cd7281a9de4f93be
Kerimsekili <kerimsekilix@gmail.com>
2024-01-06 21:48:54 +0300

    Language Support into the mail

--- investdashapi/src/main/java/com/investdash/ws/email/EmailService.java
+++ investdashapi/src/main/java/com/investdash/ws/email/EmailService.java
@@ -2,11 +2,16 @@ package com.investdash.ws.email;
 
 import com.investdash.ws.configuration.InvestdashProperties;
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Properties;
 
 @Service
@@ -16,6 +21,9 @@ public class EmailService {
 
     @Autowired
     InvestdashProperties investdashProperties;
+
+    @Autowired
+    MessageSource messageSource;
 
     @PostConstruct
     public void initialize() {
@@ -29,14 +37,39 @@ public class EmailService {
         properties.put("mail.smtp.starttls.enable", "true");
     }
 
+    String activationEmail = """
+            <html>
+            <body>
+            <h1>${title}</h1>
+            <a href="${url}">${clickHere}</a>
+            </body>
+            </html>
+            """;
+
     public void sendActivationEmail(String email, String activationToken) {
         var activationUrl = investdashProperties.getClient().host() + "/activation/" + activationToken;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(investdashProperties.getEmail().from());
-        message.setTo(email);
-        message.setSubject("Account Activation");
-        message.setText(activationUrl);
-        this.mailSender.send(message);
+        var title = messageSource.getMessage("investdash.mail.user.created.title", null, LocaleContextHolder
+                .getLocale());
+        var clickHere = messageSource.getMessage("investdash.mail.click.here", null, LocaleContextHolder.getLocale());
+
+
+        var mailBody = activationEmail
+                .replace("${url}", activationUrl)
+                .replace("${title}",title)
+                .replace("${clickHere}",clickHere);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage,"UTF-8");
+        try {
+            message.setFrom(investdashProperties.getEmail().from());
+            message.setTo(email);
+            message.setSubject("Account Activation");
+            message.setText(mailBody, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        this.mailSender.send(mimeMessage);
     }
 
 }
