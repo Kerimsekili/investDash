efe618f0e24bb0fcf2040bbcb80905528a29927c
Kerimsekili <kerimsekilix@gmail.com>
2024-03-04 21:28:35 +0300

    Adding Auth

--- investdashui/src/pages/Activation/index.jsx
+++ investdashui/src/pages/Activation/index.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { activateUser } from "./api";
 
 export function Activation() {
   const { token } = useParams();
-  return <h1>Activation</h1>;
+  const [apiProgress, setApiProgress] = useState();
+  const [successMessage, setSuccessMessage] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    async function activate() {
+      setApiProgress(true);
+      try {
+        const response = await activateUser(token);
+        setSuccessMessage(response.data.message);
+      } catch (axiosError) {
+        setErrorMessage(axiosError.response.data.message);
+      } finally {
+        setApiProgress(false);
+      }
+    }
+    activate();
+  }, []);
+
+  return (
+    <>
+      {apiProgress && (
+        <span className="spinner-border" aria-hidden="true"></span>
+      )}
+      {successMessage && (
+        <div className="alert alert-success">{successMessage}</div>
+      )}
+      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+    </>
+  );
 }
